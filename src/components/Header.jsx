a967e6e7ed30f24c95ff42cf959f7d3532b034f8
Yue Su <su2047@gmail.com>
2020-11-28 14:45:00 -0400

    basic setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@material-ui/core"
+import React from "react"
+
+const Header = () => {
+  return (
+    <Box>
+      <Typography variant="h1">Blog</Typography>
+    </Box>
+  )
+}
+
+export default Header
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core"
+
+export const theme = createMuiTheme({
+  typography: {
+    h1: {
+      fontFamily: "'Libre Baskerville', serif",
+    },
+    body1: {
+      fontFamily: "'Playfair Display', serif",
+    },
+  },
+})

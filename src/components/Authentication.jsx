9e2d7a81d84984d1145209cf9941ef9e1b39b918
Yue Su <su2047@gmail.com>
2021-01-24 20:16:04 -0400

    Update Authentication.jsx

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -2,7 +2,7 @@ import { Box, Grid } from "@material-ui/core"
 import React, { useContext } from "react"
 import { userContext } from "../providers/UsersProvider"
 import SignIn from "./SignIn"
-import SingInAndSignUp from "./SingInAndSignUp"
+//import SingInAndSignUp from "./SingInAndSignUp"
 import UserCard from "./UserCard"
 
 const Authentication = () => {
